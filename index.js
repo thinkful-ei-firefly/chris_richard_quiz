c7940c6cdb4dd2fac4d071f9d7502b809e1651df
rjgates3 <gates.j.richard@gmail.com>
2019-07-12 15:27:09 -0600

    update js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 function main() {
 
+  console.log('loaded main funciton');
+  console.log($('button').attr($('class'));
+
   let questionNum = 0; //0=begin, E=end
   let score = 0; //initial score = 0;
 
@@ -34,47 +37,74 @@ function main() {
     }
   };
 
-  //initialize <main>
-
-  /*$(main).html(`
+  //initialize <main> with start
+  $(main).html(`
         <form class="quiz" methode="post">
         <fieldset name='quiz'>
         <ledgend>Get Started</ledgend>
-        <button class='go' type='go'>Quiz Time</button>
-        `)
-    */
+        <button class='begin' type='go'>Quiz Time</button>
+        `
+  );
+
   //listener for the start quiz button id = 'begin'
-  $(main).on('click', '.begin', event => {
-    const question = dataSet.q1.text;
-    const options = dataSet.q1.options;
-    const answer = dataSet.q1.answer;
-    const a = getQuestionHtml(question, options, answer);
-    updateHtml(a);
+  $(main).on('click', '#begin', event => {
+    event.preventDefault();
+    //updating main HTML with the question 1 details.
+    updateHtml(getQuestionHtml(dataSet.q1.text, dataSet.q1.options, dataSet.q1.answer));
   });
 
   //listener for the check anwser button id = 'check'
-  $(main).on('click', '.check', event => {
-    //check answer
-    //if correct
-    //call ifCorrect function 
-    //if incorrect
-    //call ifWrong function
-    //update html
-
-
+  //checks to see if the ansewer is correct
+  //goes to wrong or correct
+  //increments questionNum by one
+  //updates score
+  $(main).on('click', '#check', event => {
+    event.preventDefault();
+    //get the answer chosen
+    const choice = $(event.target).val();
+    const result = isRight(choice);
+    if (result) {
+      let a = getCorrectHtml();
+      updateHtml(a);
+      updateScore();
+    } else {
+      let a = getWrongHtml();
+      updateHtml(a);
+    }
+    return questionNum++;
   });
 
   //listener for the next question button id = 'next'
-  $(main).on('click', '.next', event => {
-    questionNum++;
-    //update html
-
+  //the next button will be on a correct or wrong answer page
+  //update question number html
+  $(main).on('click', '#next', event => {
+    event.preventDefault();
+    if (questionNum < dataSet.length) {
+      if (questionNum === 1) { 
+        updateHtml(getQuestionHtml(dataSet.q1.text, dataSet.q1.options, dataSet.q1.answer));            
+      }else if (questionNum === 2) { 
+        updateHtml(getQuestionHtml(dataSet.q2.text, dataSet.q2.options, dataSet.q2.answer));            
+      } else if (questionNum === 3) { 
+        updateHtml(getQuestionHtml(dataSet.q3.text, dataSet.q3.options, dataSet.q3.answer));            
+      } else if (questionNum === 4) { 
+        updateHtml(getQuestionHtml(dataSet.q4.text, dataSet.q4.options, dataSet.q4.answer));            
+      } else if (questionNum === 5) { 
+        updateHtml(getQuestionHtml(dataSet.q5.text, dataSet.q5.options, dataSet.q5.answer));            
+      } else {
+        // update to End html
+        updateHtml(getEndHtml());
+      }
+    }
+    return questionNum++;
   });
 
   //listener for the restart quiz button id = 'restart'
-  $(main).on('click', '.restart', event => {
+  $(main).on('click', '#restart', event => {
+    event.preventDefault();
+    updateHtml(getStartHtml());
     questionNum = 0;
     score = 0;
+    return questionNum, score;
     //update html
   });
 
@@ -83,55 +113,77 @@ function main() {
         <form class='quiz' method='post'>
         <fieldset>${question}</fieldset>
         <input type='radio' id='a' value='AnswerA'/>
-        <label for='AnswerA'>${option[0]}</label>
+        <label for='AnswerA'>${options[0]}</label>
         <input type='radio' id='b' value='AnswerB'/>
-        <label for='AnswerB'>${option[1]}</label>
+        <label for='AnswerB'>${options[1]}</label>
         <input type='radio' id='c' value='AnswerC'/>
-        <label for='AnswerC'>${option[2]}</label>
+        <label for='AnswerC'>${options[2]}</label>
         <input type='radio' id='d' value='AnswerD'/>
-        <label for='AnswerD'>${option[3]}</label>
+        <label for='AnswerD'>${options[3]}</label>
         <input type='radio' id='e' value='AnswerE'/>
-        <label for='AnswerE'>${option[4]}</label>
-        <button type='check' class='go'>Check Answer</button
+        <label for='AnswerE'>${options[4]}</label>
+        <button type='check' id='check'>Check Answer</button
         </form>`;
+    return questionHtml;
   }
 
   function getStartHtml() {
-      const a =`
+    const a =`
       <form class="quiz" methode="post">
       <fieldset name='quiz'>
       <ledgend>Get Started</ledgend>
-      <button class='begin' type='go'>Quiz Time</button>
+      <button id='begin' type='go'>Quiz Time</button>
       `;
-      return a;
+    return a;
   }
 
   function getEndHtml() {
-      const a =`
+    const a =`
       <form class="quiz" methode="post">
       <fieldset name='quiz'>
       <ledgend>Get Started</ledgend>
-      <button class='restart' type='go'>Quiz Time</button>
-      `
+      <button id='restart' type='go'>Quiz Time</button>
+      `;
+    return a;
+  }
+
+  function getWrongHtml() {
+    const a =`
+        <form class="quiz" methode="post">
+        <fieldset name='quiz'>
+        <ledgend>Quiz</ledgend>
+        <p>Incorrect :(</p>
+        <button class='next' type='go'>Next Question</button>
+        `;
+    return a;
+  }
+
+  function getCorrectHtml() {
+    const a =`
+      <form class="quiz" methode="post">
+      <fieldset name='quiz'>
+      <ledgend>Quiz</ledgend>
+      <p>Correct!</p>
+      <button class='Next' type='go'>Next Question</button>
+      `;
+    return a;
   }
   
-  //function update question number text
-
-  function updateScore() {
-      return score++;
+  //updates the html inside of main
+  function updateHtml(htmlCode) {
+    $('main').html(htmlCode);
   }
 
-  //function ifWrong function
-    //set html to wrong answer
-    //call updateScore function is not needed
-    //call mainHtml function
+  //function update score
+  function updateScore() {
+    return score++;
+    //could update score html here
+  }
 
-  //function ifCorrect
-    //set html to correct answer
-    //call updateScore function
-    //call mainHtml function
-
-
+  //check answer function
+  function isRight(choice, answer) {
+    return true; //will need to fix this later, to check if answer is correct
+  }
 }
 
 main();
