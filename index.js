a91d9ec89e465b85c3560bceb01fc5a61e4e0adc
rjgates3 <gates.j.richard@gmail.com>
2019-07-12 11:54:56 -0600

    update js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 function main() {
 
+    let questionNum = 0; //0=begin, E=end
+    let score = 0; //initial score = 0;
+
+    //data for questions
     let dataSet = {
         q1: {
             text:'question1: a is correct',
@@ -30,9 +34,36 @@ function main() {
         }
     }
 
-//function updates html
+//initialize <main>
 
-//check to see if answer is correct
+$(main).html(`
+    <form class="quiz" methode="post">
+    <fieldset name='quiz'>
+    <ledgend>Get Started</ledgend>
+    <button class='go' type='go'>Quiz Time</button>
+    `)
+
+$(main).on('click', '.go' event => {
+
+});
+
+//function updates mainhtml
+
+//function update question number text
+
+//function update score
+
+//function check answer
+
+//function if wrong
+
+//function if correct
+
+//function check to see if answer is correct
+
+//function start quiz
+
+
 
 }
 
