b4189bafb696ece5c6088cf3a35c9a6eb880c5aa
rjgates3 <gates.j.richard@gmail.com>
2019-07-12 10:20:21 -0600

    added fake

--- index.js
+++ index.js
@@ -2,7 +2,33 @@
 
 function main() {
 
-//data set
+    let dataSet = {
+        q1: {
+            text:'question1: a is correct',
+            options: ['a', 'b', 'c', 'd'],
+            answer: 'a',
+        }
+        q2: {
+            text:'question1: b is correct',
+            options: ['a', 'b', 'c', 'd'],
+            answer: 'b',
+        }
+        q3: {
+            text:'question1: c is correct',
+            options: ['a', 'b', 'c', 'd'],
+            answer: 'c',
+        }
+        q4: {
+            text:'question1: d is correct',
+            options: ['a', 'b', 'c', 'd'],
+            answer: 'd',
+        }
+        q5: {
+            text:'question1: a is correct',
+            options: ['a', 'b', 'c', 'd'],
+            answer: 'a',
+        }
+    }
 
 //function updates html
 
